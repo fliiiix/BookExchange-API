97ea51e153dbf67de9658002ff0bd2c18219829a
jacotdev <jacotdev@student.ethz.ch>
2015-10-03 03:01:06 +0200

    Filled up book.rb and user.rb

---
+++ controller/book.rb
@@ -0,0 +1,12 @@
+get "/book/:isbn/?" do
+end
+get "/book/:list/?" do 
+end
+get "/book/:userid/?" do
+end
+get "/book/add" do
+end
+get "/book/edit" do
+end
+get "/book/remove" do
+end
---
+++ controller/user.rb
@@ -0,0 +1,14 @@
+get "/user/:id/?" do
+end
+get "/user/:username/?" do 
+end
+get "/user/:list/?" do 
+end
+get "/user/:id/books/?" do
+end
+get "/login" do 
+end
+get "/logout" do 
+end
+post "/login" do
+end
