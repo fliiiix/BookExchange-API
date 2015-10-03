4b89e4927cff97f4dfd5790af14477b86371bf46
Sir l33tname <de-ch@hotmail.de>
2015-10-03 12:11:12 +0200

    depoly to swisscom cloud

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source "https://rubygems.org"
 
+ruby '2.0.0'
+
+gem "puma"
 gem "sinatra"
 gem "dm-mysql-adapter"
 gem "data_mapper"
--- controller/main.rb
+++ controller/main.rb
@@ -6,8 +6,17 @@ configure do
   set :views, Proc.new { File.join(root, "../views") }
   set :public_folder, Proc.new { File.join(root, "../public") }
 
-  DataMapper.setup(:default, 'mysql://localhost/books')
+  
   DataMapper::Model.raise_on_save_failure = true
+end
+
+configure :development do
+  DataMapper.setup(:default, 'mysql://localhost/books')
+end
+
+configure :production do
+  db = JSON.parse(ENV['VCAP_SERVICES'])['mariadb'][0]['credentials']['database_uri']
+  DataMapper.setup(:default, db)
 end
 
 require_relative "../model/main"
