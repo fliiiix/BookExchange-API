dd730a46a2d3be7fee6ae98276674387e0485c1c
Valentin <jacotdev@student.ethz.ch>
2015-10-03 20:28:08 +0200

    fake.rb filled up with sample

--- model/fake.rb
+++ model/fake.rb
@@ -1,4 +1,8 @@
-#just fake all the data
+"1856694739" "Book Design" "Andrew Haslam" "http://farm1.static.flickr.com/134/379780428_6b73b0e854.jpg"
 
-u = User.create(:username => "testuser", :password => "1234", :mail => "testuser@gmail.com", :plz => 6000) 
-Token.create(:token => "XXXYYYXXX", :user => u)
+"142992246X" "Beneath the Book Tower" "Steve Hamilton" "http://ecx.images-amazon.com/images/I/51QZQGdqtbL._SX374_BO1,204,203,200_.jpg"
+
+"3764355123" "Une Petite Maison" "Le Corbusier" "http://www.arteum.com/6069-6538-thickbox/magnet-une-petite-maison-collection-le-corbusier.jpg"
+
+"9635257961" "The Great Gatsby" "Francis Scott Fitzgerald" "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Gatsby_1925_jacket.gif/220px-Gatsby_1925_jacket.gif"
+
